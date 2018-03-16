64fdb13e06ba8a9b1c9e90feafd54a120b068721
Kristofer Wallöf <kristofer.wallof@spotonsolutions.se>
2018-03-16 13:29:38 +0100

    git init

---
+++ src/js/index.js
@@ -0,0 +1,62 @@
+const contentful = require('contentful');
+
+const client = contentful.createClient({
+    space: 'eyl63nxyj988',
+    accessToken: '985c4f3711f8c93802f8d08aed270376ea3b21a72834403d1f0d1bf91c065a86'
+  });
+
+// client.getEntry('4jNEz18sne02AQccW8ICkK')
+//     .then(function (entry) {
+//   // logs the entry metadata
+//     console.log(entry.fields.title)
+// })
+
+// client.getEntries()
+// .then(function (entries) {
+//   // log the title for all the entries that have it
+//   entries.items.forEach(function (entry) {
+//     if(entry.fields.title) {
+//       console.log(entry.fields.title)
+//     }
+//   })
+// })
+
+// client.getEntries()
+//     .then(function (entries) {
+//         entries.items.forEach(function (entry) {
+//             if(entry.fields.similarShoes) {
+//                 entry.fields.similarShoes.forEach((similar) => {
+//                     if(similar.fields.productImage) {
+//                         console.log(similar.fields.productImage.fields.file.url);
+//                     }
+//                 })
+//             }
+//         })
+//     })
+
+// client.getEntries({include: 0})
+//     .then(function (entries) {
+//         entries.items.forEach(function (entry) {
+//             if(entry.fields.similarShoes) {
+//                 entry.fields.similarShoes.forEach((similar) => {
+//                     if(similar.fields) {
+//                         console.log(similar);
+//                     }
+//                 })
+//             }
+//         })
+//     })
+
+
+client.getEntries( {
+    'content_type': 'shirts'
+
+})
+.then(function (entries) {
+  // log the title for all the entries that have it
+  entries.items.forEach(function (entry) {
+    if(entry.fields.title) {
+      console.log(entry.fields.title)
+    }
+  })
+})
